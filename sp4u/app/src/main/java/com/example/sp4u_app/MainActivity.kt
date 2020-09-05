4255c17111154bed52eaaee926f349ddd9af9336
EmanuelleAA7 <emanuelle.araujoa7@gmail.com>
2020-09-04 21:59:35 -0300

    Adicionando primerias configurações no projeto #3

---
+++ sp4u/app/src/main/java/com/example/sp4u_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sp4u_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
