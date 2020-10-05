9bdcb22864d9f74b8606e5861384cfff693059dc
Bianca Barranco <bianca.barranco1808@gmail.com>
2020-10-05 20:59:11 -0300

    Criando as telas de edição de senha e e-mail do usuário

---
+++ sp4u/app/src/main/java/com/example/sp4u_app/UserProfile2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.sp4u_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserProfile2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile_2)
+    }
+}
