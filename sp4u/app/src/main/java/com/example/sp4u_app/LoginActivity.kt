223b1cd600168cfddd2e67d4a855417a2169b393
Victor Massao <victor.massao@cerc.inf.br>
2020-10-12 19:29:36 -0300

    Criado estrutura para requisicoes
    - Criado toda a estrutura de login, fazendo requisicao no autenticador
    - Corrigido telas que nao estava compilando
    - Adicionado validador para edit texts vazias

--- sp4u/app/src/main/java/com/example/sp4u_app/LoginActivity.kt
+++ sp4u/app/src/main/java/com/example/sp4u_app/LoginActivity.kt
@@ -3,14 +3,57 @@ package com.example.sp4u_app
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sp4u_app.dto.request.AuthenticateRequestDTO
+import com.example.sp4u_app.dto.response.AuthenticateResponseDTO
+import com.example.sp4u_app.utils.RetrofitInitializer
+import com.example.sp4u_app.validator.validator
+import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
-    fun goToForgotPassword(button:View){
+
+    fun login(component: View) {
+        if(!validator(listOf(et_email, et_senha))) {
+            return
+        }
+        val authenticate = AuthenticateRequestDTO(et_email.text.toString(), et_senha.text.toString())
+        val call =
+            RetrofitInitializer("http://ec2-100-26-215-123.compute-1.amazonaws.com:8080/")
+                .authenticateService()
+                .authenticate(authenticate)
+
+        call.enqueue(object: Callback<AuthenticateResponseDTO?> {
+            override fun onResponse(call: Call<AuthenticateResponseDTO?>?,
+                                    response: Response<AuthenticateResponseDTO?>?) {
+                if(response?.code() == 200) {
+                    response?.body()?.let {
+                        val body: AuthenticateResponseDTO = it
+                        println(body)
+                        goToHome()
+                    }
+                } else if (response?.code() == 404) {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        resources.getString(R.string.login_errado),
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onFailure(call: Call<AuthenticateResponseDTO?>?,
+                                   t: Throwable?) {
+                println(t.toString())
+            }
+        })
+    }
+
+    fun goToForgotPassword(button: View){
         val forgotPassword = Intent(this, ForgotPasswordActivity::class.java)
         startActivity(forgotPassword)
     }
@@ -20,7 +63,7 @@ class LoginActivity : AppCompatActivity() {
         startActivity(register)
     }
 
-    fun goToHome2(button:View){
+    private fun goToHome(){
         val home = Intent(this, HomeActivity::class.java)
         startActivity(home)
     }
