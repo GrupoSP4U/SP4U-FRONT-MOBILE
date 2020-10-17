d2d1bec77307b39834d663dd2091b2ce858457ce
EmanuelleAA7 <emanuelle.araujoa7@gmail.com>
2020-10-17 18:14:49 -0300

    cria primeira tela de edição de estabelecimento

---
+++ sp4u/app/src/main/java/com/example/sp4u_app/RegisterEstabilishmentConfigActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sp4u_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterEstabilishmentConfigActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_estabilishment_config)
+    }
+}
