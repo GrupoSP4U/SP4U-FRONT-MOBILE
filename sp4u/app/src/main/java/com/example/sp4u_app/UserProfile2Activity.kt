8f300199254edd4127475d8ad664641eda713bde
Bianca Barranco <bianca.barranco1808@gmail.com>
2020-10-21 19:47:42 -0300

    Criando as funções de link entre as telas de edição de perfil do usuário

--- sp4u/app/src/main/java/com/example/sp4u_app/UserProfile2Activity.kt
+++ sp4u/app/src/main/java/com/example/sp4u_app/UserProfile2Activity.kt
@@ -1,11 +1,23 @@
 package com.example.sp4u_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class UserProfile2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile_2)
+    }
+
+    fun goToBasicSettings(button: View){
+        val register = Intent(this, UserProfileActivity::class.java)
+        startActivity(register)
+    }
+
+    fun goToPasswordEditing(button: View){
+        val register = Intent(this, UserProfile3Activity::class.java)
+        startActivity(register)
     }
 }
--- sp4u/app/src/main/java/com/example/sp4u_app/UserProfileActivity.kt
+++ sp4u/app/src/main/java/com/example/sp4u_app/UserProfileActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sp4u_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class UserProfileActivity : AppCompatActivity() {
@@ -8,5 +10,15 @@ class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+    }
+
+    fun goToEmailEditing(button: View){
+        val register = Intent(this, UserProfile2Activity::class.java)
+        startActivity(register)
+    }
+
+    fun goToPasswordEditing(button: View){
+        val register = Intent(this, UserProfile3Activity::class.java)
+        startActivity(register)
     }
 }
