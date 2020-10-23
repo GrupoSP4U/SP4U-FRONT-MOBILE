6135537409a4de0e8d8a9cd4ef31a4ec39d6bff2
lucasmessiasbandtec <lucas.messias@bandtec.com.br>
2020-10-22 21:06:35 -0300

    Add menu na home

--- sp4u/app/src/main/java/com/example/sp4u_app/HomeActivity.kt
+++ sp4u/app/src/main/java/com/example/sp4u_app/HomeActivity.kt
@@ -17,9 +17,28 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
-     fun goToHumor(button: View) {
+    //    menu config
+
+    fun goToHomeAuthenticated(button: View) {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToHumor(button: View) {
         val intent = Intent(this, FeelsActivity::class.java)
         startActivity(intent)
     }
+
+    fun goToBasicSettings(button: View){
+        val register = Intent(this, UserProfileActivity::class.java)
+        startActivity(register)
+    }
+
+    fun goToEnterprise(button: View){
+        val register = Intent(this, RegisterEstablishmentActivity::class.java)
+        startActivity(register)
+    }
+
+//    fim menu config
 
 }
