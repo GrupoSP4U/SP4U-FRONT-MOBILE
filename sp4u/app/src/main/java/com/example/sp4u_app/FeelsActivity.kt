49c9e2dbb298949c42b86ebd9f28652b22b680a3
lucasmessiasbandtec <lucas.messias@bandtec.com.br>
2020-10-22 21:29:52 -0300

    add redirec no card de humor

--- sp4u/app/src/main/java/com/example/sp4u_app/FeelsActivity.kt
+++ sp4u/app/src/main/java/com/example/sp4u_app/FeelsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sp4u_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class FeelsActivity : AppCompatActivity() {
@@ -8,4 +10,29 @@ class FeelsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feels)
     }
+
+    //    menu config
+
+    fun goToHomeAuthenticated(button: View) {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToHumor(button: View) {
+        val intent = Intent(this, FeelsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToBasicSettings(button: View){
+        val register = Intent(this, UserProfileActivity::class.java)
+        startActivity(register)
+    }
+
+    fun goToEnterprise(button: View){
+        val register = Intent(this, RegisterEstablishmentActivity::class.java)
+        startActivity(register)
+    }
+
+//    fim menu config
+
 }
