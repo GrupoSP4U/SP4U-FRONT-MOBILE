11ba61cc7f038424714ad443733fecbbe4eaec4d
Victor Massao <vimassao@gmail.com>
2020-10-25 19:46:38 -0300

    Fix core url

--- sp4u/app/src/main/java/com/example/sp4u_app/utils/RetrofitInitializer.kt
+++ sp4u/app/src/main/java/com/example/sp4u_app/utils/RetrofitInitializer.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInitializer(
     token: String? = "",
-    path: String = "http://192.168.15.136:8081/"
+    path: String = "http://ec2-34-229-98-39.compute-1.amazonaws.com:8081/"
 ) {
     private val interceptor = TokenInterceptor(token)
     private val client: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
